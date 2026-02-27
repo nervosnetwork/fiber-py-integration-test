0597baeb6e1961e278de90eabb51abfbd6008fa6
gpBlockchain <32102187+gpBlockchain@users.noreply.github.com>
2026-02-27 15:24:33 +0800

    mainnet ci test

--- test_cases/fiber/mainnet/test_fiber.py
+++ test_cases/fiber/mainnet/test_fiber.py
@@ -1,28 +1,25 @@
 import os
-import random
 import time
 
 from framework.basic import CkbTest
-from framework.basic_fiber import FiberTest
 from framework.fiber_rpc import FiberRPCClient
-from framework.rpc import RPCClient
 from framework.test_fiber import Fiber, FiberConfigPath
-from framework.util import generate_random_preimage
-import logging
 
-from test_cases.fiber.devnet.settle_invoice.test_settle_invoice import sha256_hex
+import logging
 
 LOGGER = logging.getLogger(__name__)
 
 
-# ckt1qzda0cr08m85hc8jlnfp3zer7xulejywt49kt2rr0vthywaa50xwsqt2yg5ctyv59wsrqk2d634rj6k7c8kdjycft39my
-# ckt1qzda0cr08m85hc8jlnfp3zer7xulejywt49kt2rr0vthywaa50xwsq0n4lwpc3k24hnt75pmgpmg2hgack50wdgnlsp6m
-
-
 class TestFiber(CkbTest):
-    # cryptapeFiber1 = FiberRPCClient("http://18.163.221.211:8227")
-    # cryptapeFiber2 = FiberRPCClient("http://18.162.235.225:8227")
-    ckbClient = RPCClient("https://mainnet.ckb.dev")
+    cryptapeFiber1_peer_address = (
+        "/ip4/52.52.69.223/tcp/8228/p2p/QmZCfzENZqWrWwifJj9BFDvxQWFyYw5GjdB4vN7Ynd4FxY"
+    )
+    cryptapeFiber1_peer_id = "QmZCfzENZqWrWwifJj9BFDvxQWFyYw5GjdB4vN7Ynd4FxY"
+
+    cryptapeFiber2_peer_address = (
+        "/ip4/54.178.252.1/tcp/8228/p2p/QmZ73KHvZ5GFxf6XhHZ3icPeKFo93rk86kZ8qauox3avJP"
+    )
+    cryptapeFiber2_peer_id = "QmZ73KHvZ5GFxf6XhHZ3icPeKFo93rk86kZ8qauox3avJP"
 
     ACCOUNT_PRIVATE_1 = os.getenv("ACCOUNT_PRIVATE_1")
     ACCOUNT_PRIVATE_2 = os.getenv("ACCOUNT_PRIVATE_2")
@@ -56,9 +53,12 @@ class TestFiber(CkbTest):
         cls.fiber2.start()
 
         cls.fiber1.get_client().connect_peer(
-            {"address": cls.fiber2.get_client().node_info()["addresses"][0]}
+            {"address": cls.cryptapeFiber1_peer_address}
         )
 
+        cls.fiber2.get_client().connect_peer(
+            {"address": cls.cryptapeFiber2_peer_address}
+        )
         time.sleep(10)
 
     @classmethod
@@ -80,7 +80,7 @@ class TestFiber(CkbTest):
                 }
             )
             wait_for_channel_state(
-                cls.fiber1.get_client(), cls.fiber2.get_peer_id(), "CLOSED", 360
+                cls.fiber1.get_client(), cls.cryptapeFiber1_peer_id, "CLOSED", 120
             )
 
         channels = cls.fiber2.get_client().list_channels({})
@@ -100,7 +100,7 @@ class TestFiber(CkbTest):
                 }
             )
             wait_for_channel_state(
-                cls.fiber2.get_client(), cls.fiber2.get_peer_id(), "CLOSED", 120
+                cls.fiber2.get_client(), cls.cryptapeFiber2_peer_id, "CLOSED", 120
             )
 
         cls.fiber1.stop()
@@ -108,94 +108,60 @@ class TestFiber(CkbTest):
         cls.fiber2.stop()
         cls.fiber2.clean()
 
-    # def test_settle_tx(self):
-    #     # temporary_channel_id = self.fiber1.get_client().open_channel(
-    #     #     {
-    #     #         "peer_id": self.fiber2.get_peer_id(),
-    #     #         "funding_amount": hex(1000 * 100000000),
-    #     #         "public": True,
-    #     #         # "tlc_fee_proportional_millionths": "0x4B0",
-    #     #     }
-    #     # )
-    #     # time.sleep(10)
-    #     # wait_for_channel_state(
-    #     #     self.fiber1.get_client(),
-    #     #     self.fiber2.get_peer_id(),
-    #     #     "CHANNEL_READY",
-    #     #     360,
-    #     # )
-    #     #
-    #     # begin = time.time()
-    #     # # wait dry_run success
-    #     # send_payment(
-    #     #     self.fiber1.get_client(), self.fiber2.get_client(), 1000, None, 20 * 60
-    #     # )
-    #     # fiber1_to_fiber2_time = time.time()
-    #     # send_payment(
-    #     #     self.fiber2.get_client(), self.fiber1.get_client(), 1000, None, 20 * 60
-    #     # )
-    #     # fiber2_to_fiber1_time = time.time()
-    #     # LOGGER.info(f"fiber1_to_fiber2 cost time: {fiber1_to_fiber2_time - begin}")
-    #     # LOGGER.info(
-    #     #     f"fiber2_to_fiber1 cost time: {fiber2_to_fiber1_time - fiber1_to_fiber2_time}"
-    #     # )
-    #
-    #     # settle tx
-    #     preimage = generate_random_preimage()
-    #     payment_hash = sha256_hex(preimage)
-    #     print("payment_hash", payment_hash)
-    #     print("preimage", preimage)
-    #     invoice = self.fiber2.get_client().new_invoice(
-    #         {
-    #             "amount": hex(1 * 100000000),
-    #             "currency": "Fibb",
-    #             "description": "open invoice settle should fail",
-    #             "payment_hash": payment_hash,
-    #             "hash_algorithm": "sha256",
-    #         }
-    #     )
-    #     payment = self.fiber1.get_client().send_payment({
-    #         "invoice": invoice["invoice_address"],
-    #
-    #     })
-    #     time.sleep(10)
-    #     # check payment is receive
-    #     # force shutdown
-    #     channel = self.fiber1.get_client().list_channels({})['channels'][0]
-    #     self.fiber1.get_client().shutdown_channel(
-    #         {
-    #             "channel_id": channel["channel_id"],
-    #             "force": True,
-    #         }
-    #     )
-    #     # wait tx submit
-    #     time.sleep(60)
-    #     # settle tx
-    #     self.fiber2.get_client().settle_invoice({
-    #         "payment_hash": payment["payment_hash"],
-    #         "payment_preimage": preimage,
-    #     })
-
     def test_ckb_01(self):
         # open_channel
         temporary_channel_id = self.fiber1.get_client().open_channel(
             {
-                "peer_id": self.fiber2.get_peer_id(),
-                "funding_amount": hex(200 * 100000000),
+                "peer_id": self.cryptapeFiber1_peer_id,
+                "funding_amount": hex(1000 * 100000000),
                 "public": True,
                 # "tlc_fee_proportional_millionths": "0x4B0",
             }
         )
-        time.sleep(10)
+
+        temporary_channel_id = self.fiber2.get_client().open_channel(
+            {
+                "peer_id": self.cryptapeFiber2_peer_id,
+                "funding_amount": hex(1000 * 100000000),
+                "public": True,
+                # "tlc_fee_proportional_millionths": "0x4B0",
+            }
+        )
         wait_for_channel_state(
             self.fiber1.get_client(),
-            self.fiber2.get_peer_id(),
+            self.cryptapeFiber1_peer_id,
             "CHANNEL_READY",
-            360,
+            120,
         )
 
+        wait_for_channel_state(
+            self.fiber2.get_client(),
+            self.cryptapeFiber2_peer_id,
+            "CHANNEL_READY",
+            120,
+        )
         begin = time.time()
         # wait dry_run success
+        send_payment(
+            self.fiber1.get_client(), self.fiber2.get_client(), 1000, None, 20 * 60
+        )
+        fiber1_to_fiber2_time = time.time()
+        send_payment(
+            self.fiber2.get_client(), self.fiber1.get_client(), 1000, None, 20 * 60
+        )
+        fiber2_to_fiber1_time = time.time()
+        LOGGER.info(f"fiber1_to_fiber2 cost time: {fiber1_to_fiber2_time - begin}")
+        LOGGER.info(
+            f"fiber2_to_fiber1 cost time: {fiber2_to_fiber1_time - fiber1_to_fiber2_time}"
+        )
+
+    def test_ckb_02(self):
+        self.fiber1.stop()
+        self.fiber2.stop()
+        self.fiber1.start()
+        self.fiber2.start()
+        time.sleep(15)
+        begin = time.time()
         send_payment(
             self.fiber1.get_client(), self.fiber2.get_client(), 1000, None, 20 * 60
         )
@@ -245,13 +211,6 @@ def send_payment(
             print(f"wait try count: {i}")
             continue
     raise TimeoutError("payment timeout")
-
-
-def generate_random_preimage():
-    hash_str = "0x"
-    for _ in range(64):
-        hash_str += hex(random.randint(0, 15))[2:]
-    return hash_str
 
 
 def wait_for_channel_state(client, peer_id, expected_state, timeout=120):
