3caf4b4de028968839703fa11f8d6b393087a498
15168316096 <15168316096@163.com>
2026-03-16 18:03:56 +0800

    add flow fiber

--- test_cases/conftest.py
+++ test_cases/conftest.py
@@ -1,6 +1,15 @@
 import pytest
 
 
+def pytest_addoption(parser):
+    parser.addoption(
+        "--run-remote",
+        action="store_true",
+        default=False,
+        help="Run tests that require access to remote authenticated Fiber nodes",
+    )
+
+
 @pytest.hookimpl(tryfirst=True, hookwrapper=True)
 def pytest_runtest_makereport(item, call):
     outcome = yield
