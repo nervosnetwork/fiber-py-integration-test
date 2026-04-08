ce6770a8ddf767ead303e4d53b3b30157c8fd654
15168316096 <15168316096@163.com>
2026-04-08 17:06:21 +0800

    fix not yet merged

--- test_cases/fiber/devnet/fnn-cli/test_cli_udt_funding_retry.py
+++ test_cases/fiber/devnet/fnn-cli/test_cli_udt_funding_retry.py
@@ -17,6 +17,7 @@ import pytest
 from framework.basic_fiber import FiberTest
 from framework.fnn_cli import FnnCli
 
+
 @pytest.mark.fnn_cli
 @pytest.mark.skip(reason="PR-1253 fix not yet merged - see #1195")
 class TestCliUdtFundingAbortNoUdtCells(FiberTest):
