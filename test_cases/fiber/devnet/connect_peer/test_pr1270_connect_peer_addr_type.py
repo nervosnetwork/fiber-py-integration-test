edb1f6ff1002885bd8c084dd8c608d02803bc155
gpBlockchain <744158715@qq.com>
2026-04-16 18:17:56 +0800

    remove unused code

--- test_cases/fiber/devnet/connect_peer/test_pr1270_connect_peer_addr_type.py
+++ test_cases/fiber/devnet/connect_peer/test_pr1270_connect_peer_addr_type.py
@@ -23,28 +23,6 @@ class TestPR1270ConnectPeerAddrType(SharedFiberTest):
         self.fiber1.connect_peer(self.fiber3)
         time.sleep(1)
 
-        addr = self.fiber3.get_client().node_info()["addresses"][0]
-        addr = addr.replace("0.0.0.0", "127.0.0.1").replace("0。0.0.0", "127.0.0.1")
-        i = addr.index("/p2p/")
-        wss_addr = addr[:i] + "/wss" + addr[i:]
-
-        self.fiber3.stop()
-        dh, di = self.udtContract.get_deploy_hash_and_index()
-        cfg = {
-            "ckb_rpc_url": self.node.rpcUrl,
-            "ckb_udt_whitelist": True,
-            "xudt_script_code_hash": self.Contract.get_ckb_contract_codehash(
-                dh, di, True, self.node.rpcUrl
-            ),
-            "xudt_cell_deps_tx_hash": dh,
-            "xudt_cell_deps_index": di,
-            "fiber_announced_addrs": [wss_addr],
-        }
-        cfg.update(self.start_fiber_config)
-        self.fiber3.prepare(update_config=cfg)
-        self.fiber3.start(fnn_log_level=self.fnn_log_level)
-        self.fiber1.connect_peer(self.fiber3)
-
         self.__class__.fiber4 = self.start_new_fiber(generate_account_privakey())
         self.fiber1.connect_peer(self.fiber4)
 
@@ -62,7 +40,9 @@ class TestPR1270ConnectPeerAddrType(SharedFiberTest):
     def test_connect_pubkey_addr_type_ws_uses_ws_address(self):
         for p in self.fiber2.get_client().list_peers()["peers"]:
             if p["pubkey"] == self.fiber3.get_pubkey():
-                self.fiber2.get_client().disconnect_peer({"pubkey": self.fiber3.get_pubkey()})
+                self.fiber2.get_client().disconnect_peer(
+                    {"pubkey": self.fiber3.get_pubkey()}
+                )
                 time.sleep(1)
                 break
 
