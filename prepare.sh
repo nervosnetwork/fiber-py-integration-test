4dda4ca891a1812e67d9f4db8ac1d21ee4b2cfd3
gpBlockchain <744158715@qq.com>
2026-05-29 11:01:27 +0800

    update prepare

--- prepare.sh
+++ prepare.sh
@@ -9,9 +9,6 @@ GitFIBERUrl="${GitUrl:-$DEFAULT_FIBER_URL}"
 cp download/0.202.0/ckb-cli ./source/ckb-cli
 git clone -b $GitFIBERBranch $GitFIBERUrl
 cd fiber
-cargo build
-cp target/debug/fnn ../download/fiber/current/fnn.debug
-cp target/debug/fnn-cli ../download/fiber/current/fnn-cli.debug
 cargo build --release
 cp target/release/fnn ../download/fiber/current/fnn
 cp target/release/fnn-cli ../download/fiber/current/fnn-cli
